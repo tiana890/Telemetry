2e27bba9466101703ffcb51e3bd41f56fd5a21f4
Marina <marinazayceva89@gmail.com>
2016-08-01 14:48:53 +0500

    Add POIItem

---
+++ frameworks/GoogleMaps-IOS-Utils/POIItem.h
@@ -0,0 +1,20 @@
+//
+//  POIItem.h
+//  Telemetry
+//
+//  Created by Agentum on 01.08.16.
+//  Copyright © 2016 GBU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "Clustering/GMUClusterItem.h"
+
+@interface POIItem : NSObject<GMUClusterItem>
+
+@property (nonatomic) NSString* name;
+@property (nonatomic) CLLocationCoordinate2D position;
+@property (nonatomic) NSString *prevLat;
+@property (nonatomic) NSString *prevLon;
+@property (nonatomic) NSNumber *azimut;
+@end
