470fbe3fae3136e0e793e93e092905d1168cbfca
tiana890 <minizayka@gmail.com>
2016-09-19 10:04:05 +0500

    JASON added

---
+++ Telemetry/ProgressHUD.swift
@@ -0,0 +1,13 @@
+//
+//  ProgressHUD.swift
+//  Telemetry
+//
+//  Created by IMAC  on 18.09.16.
+//  Copyright © 2016 GBU. All rights reserved.
+//
+
+import Cocoa
+
+class ProgressHUD: BaseResponse {
+
+}
